105cbb28908f1ede2a5aa39284ac59394291bf9b
sverlova <sverlova.dv@phystech.edu>
2021-05-03 04:20:32 +0300

    st build fixed

--- maze_builder/spanning_tree_build.py
+++ maze_builder/spanning_tree_build.py
@@ -1,21 +1,22 @@
 from maze_builder.maze import *
+import random
 
 class STMaze(Maze):
 # uses Kruskal's algorithm with random edges' weights
 
     def get_parent(self, a):
-        if self.parents[a] == a:
+        if self.parents[a[0]][a[1]] == a:
             return a
         else:
-            self.parents[a] = self.get_parent(self.parents[a])
-            return self.parents[a]
+            self.parents[a[0]][a[1]] = self.get_parent(self.parents[a[0]][a[1]])
+            return self.parents[a[0]][a[1]]
 
     def unite(self, a, b):
-        a = get_parent(a)
-        b = get_parent(b)
+        a = self.get_parent(a)
+        b = self.get_parent(b)
         if a == b:
             return False
-        self.parents[a] = b
+        self.parents[a[0]][a[1]] = b
         return True
 
 
@@ -27,7 +28,7 @@ class STMaze(Maze):
         edges = []
         for i in range(1, self.shape[0], 2):
             for j in range(1, self.shape[1], 2):
-                for delta in directions[:2]:
+                for delta in Maze.directions[:2]:
                     x = i + delta[0]
                     y = j + delta[1]
                     if x > 0 and y > 0 and x < self.shape[0] and y < self.shape[1]:
@@ -39,3 +40,5 @@ class STMaze(Maze):
             x, y = edge[1:]
             if self.unite(x, y):
                 self.graph[(x[0] + y[0]) // 2][(x[1] + y[1]) // 2] = 1
+
+        self.graph[0][1] = self.graph[-1][-2] = 1
